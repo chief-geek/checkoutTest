2b3afefe3876704f3a4ba99b41d2421c4bb2e67c
JHitchhiker <jai.holloway@hotmail.co.za>
2022-10-21 00:15:51 +0100

    changed bankstub to single statement

--- CreditCardVerification/Processor/BankStub.cs
+++ CreditCardVerification/Processor/BankStub.cs
@@ -14,31 +14,16 @@ namespace PaymentGateway.Processor
 
         private Task<bool> CheckCard(CreditCard? creditCard)
         {
-            if (creditCard == null || string.IsNullOrEmpty(creditCard.CardNumber))
-            {
-                return Task.FromResult(false); 
-            }
-            if (string.IsNullOrEmpty(creditCard.CardHolder))
-            {
-                return Task.FromResult(false);
-            }
-            if (creditCard.CVV == null || creditCard.CVV.Length != 3)
-            {
-                return Task.FromResult(false);
-            }
-            if (Convert.ToInt32(creditCard.Year.Substring(0,2)) < Convert.ToInt32(DateTime.Now.Year.ToString().Substring(2,2)))
-            {
-                return Task.FromResult(false);
-            }
-            if ((Convert.ToInt32(creditCard.Month) == DateTime.Now.Year) && (Convert.ToInt32(DateTime.Now.Month) < Convert.ToInt32(creditCard.Month)))
-            {
-                return Task.FromResult(false);
-            }
-            if (Convert.ToInt32(creditCard.Month) > 12)
-            {
-                return Task.FromResult(false);
-            }
-            return Task.FromResult(true);
+            var result =
+                (creditCard == null || string.IsNullOrEmpty(creditCard.CardNumber)) ||
+                (string.IsNullOrEmpty(creditCard.CardHolder)) ||
+                (creditCard.CVV == null || creditCard.CVV.Length != 3) ||
+                (Convert.ToInt32(creditCard.Year.Substring(0, 2)) < Convert.ToInt32(DateTime.Now.Year.ToString().Substring(2, 2))) ||
+                (Convert.ToInt32(creditCard.Month) == DateTime.Now.Year) && (Convert.ToInt32(DateTime.Now.Month) < Convert.ToInt32(creditCard.Month)) ||
+                (Convert.ToInt32(creditCard.Month) > 12);
+
+            return Task.FromResult(!result);
+
         }
     }
 }
